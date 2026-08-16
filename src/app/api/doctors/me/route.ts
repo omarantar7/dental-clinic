634b43cd171656d9003cdba5d022a2ef69ca73c1
Omar Antar <omarantar520@gmail.com>
2026-08-16 17:32:49 +0300

    feat: doctor update profile info

--- src/app/api/doctors/me/route.ts
+++ src/app/api/doctors/me/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { DoctorService } from "@/services/doctor.service";
+import { UpdateDoctorProfileSchema } from "@/types/doctor";
 import { requireAuth } from "@/lib/auth-guard";
 import { handleApiError } from "@/lib/handle-api-error";
 
@@ -15,3 +16,27 @@ export async function GET(request: NextRequest) {
   }
 }
 
+export async function PATCH(request: NextRequest) {
+  const authUser = requireAuth(request, ["DOCTOR"]);
+  if (authUser instanceof NextResponse) return authUser;
+
+  const body = await request.json();
+  const parsedData = UpdateDoctorProfileSchema.safeParse(body);
+
+  if (!parsedData.success) {
+    return NextResponse.json(
+      { message: "Invalid data", errors: parsedData.error.flatten() },
+      { status: 400 },
+    );
+  }
+
+  try {
+    const profile = await DoctorService.updateMyProfile(
+      authUser.userId,
+      parsedData.data,
+    );
+    return NextResponse.json(profile);
+  } catch (error) {
+    return handleApiError(error);
+  }
+}
