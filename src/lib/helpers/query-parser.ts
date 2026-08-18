bbd2f0e2534834b201133eccc465e178f86fd7cb
Omar Antar <omarantar520@gmail.com>
2026-08-18 14:43:08 +0300

    adding general parserListQuerry type

--- src/lib/helpers/query-parser.ts
+++ src/lib/helpers/query-parser.ts
@@ -3,3 +3,11 @@ import qs from "qs";
 export function parseQueryString(search: string): Record<string, any> {
   return qs.parse(search.startsWith("?") ? search.slice(1) : search);
 }
+
+export interface ParsedListQuery {
+  page: number;
+  limit: number;
+  sortBy: string;
+  sortOrder: "asc" | "desc";
+  where: Record<string, any>;
+}
