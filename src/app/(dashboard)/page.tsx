fa8407f064bd5153ac14e3b52251eecebb59d21d
Omar Antar <omarantar520@gmail.com>
2026-09-10 16:32:15 +0300

    feat: add dashboard route group with home and patients pages

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,3 @@
-// export const dynamic = "force-dynamic";
-
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
